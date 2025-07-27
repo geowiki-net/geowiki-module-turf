fad0bf16020d8d785aa9eb1e58232287b226c3b0
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2025-07-27 10:34:07 +0200

    Common function for filters with a global list of available functions

--- src/turf.js
+++ src/turf.js
@@ -1,36 +1,45 @@
 import * as turf from '@turf/turf'
 import twig from 'twig'
 
-twig.extendFilter('geomBuffer', function (value, param) {
-  let geometry = JSON.parse(value)
+const filters = {
+  geomBuffer: {
+    func: turf.buffer
+  },
+  geomConcave: {
+    func: turf.concave,
+    prepare: 'convertToFeatureCollection'
+  }
+}
 
-  let result = turf.buffer(geometry, param[0], param[1])
-
-  if (result.type === 'FeatureCollection') {
-    result = {
-      type: 'GeometryCollection',
-      geometries: result.features.map(feature => {
-        return feature.geometry
-      })
+Object.entries(filters).forEach(([ name, def ]) => {
+  twig.extendFilter(name, function (value, param) {
+    if (!value) {
+      return null
     }
-  } else {
-    result = result.geometry
-  }
 
-  return twig.filters.raw(JSON.stringify(result))
-})
+    let geometry = JSON.parse(value)
 
-twig.extendFilter('geomConcave', function (value, param) {
-  let geometry = JSON.parse(value)
+    if (def.prepare === 'convertToFeatureCollection') {
+      geometry = toFeatureCollection(geometry)
+    }
 
-  const collection = toFeatureCollection(geometry)
-  const result = turf.concave(collection, param[0])
+    let result = def.func(geometry, ...(param || []))
 
-  if (result) {
-    return twig.filters.raw(JSON.stringify(result.geometry))
-  }
+    if (!result) {
+      return null
+    } else if (result.type === 'FeatureCollection') {
+      result = {
+        type: 'GeometryCollection',
+        geometries: result.features.map(feature => {
+          return feature.geometry
+        })
+      }
+    } else {
+      result = result.geometry
+    }
 
-  return value
+    return twig.filters.raw(JSON.stringify(result))
+  })
 })
 
 function toFeatureCollection (geometry) {
