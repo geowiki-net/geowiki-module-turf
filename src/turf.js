571fc978d7f1228dc5ae804c6b6aaa0ba04274f5
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2025-07-27 10:22:36 +0200

    Add as examples geomBuffer and geomConcave

--- src/turf.js
+++ src/turf.js
@@ -1,10 +1,55 @@
-import turf from '@turf/turf'
+import * as turf from '@turf/turf'
 import twig from 'twig'
 
 twig.extendFilter('geomBuffer', function (value, param) {
   let geometry = JSON.parse(value)
 
-  geometry = turf.buffer(geometry, param[0])
+  let result = turf.buffer(geometry, param[0], param[1])
 
-  return twig.filters.raw(JSON.stringify(geometry))
+  if (result.type === 'FeatureCollection') {
+    result = {
+      type: 'GeometryCollection',
+      geometries: result.features.map(feature => {
+        return feature.geometry
+      })
+    }
+  } else {
+    result = result.geometry
+  }
+
+  return twig.filters.raw(JSON.stringify(result))
 })
+
+twig.extendFilter('geomConcave', function (value, param) {
+  let geometry = JSON.parse(value)
+
+  const collection = toFeatureCollection(geometry)
+  const result = turf.concave(collection, param[0])
+
+  if (result) {
+    return twig.filters.raw(JSON.stringify(result.geometry))
+  }
+
+  return value
+})
+
+function toFeatureCollection (geometry) {
+  if (geometry.type === 'GeometryCollection') {
+    return {
+      type: 'FeatureCollection',
+      features: geometry.geometries.map(geometry => {
+        return {
+          type: 'Feature',
+          properties: {},
+          geometry
+        }
+      })
+    }
+  }
+
+  return {
+    type: 'Feature',
+    properties: {},
+    geometry
+  }
+}
